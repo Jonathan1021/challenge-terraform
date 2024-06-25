15d96a92ff42ba873dd071a754c4655153f98d2e
Pragma <pragma@192.168.1.6>
2024-06-25 11:30:05 -0500

    Creación de Subnet Privadas

--- main.tf
+++ main.tf
@@ -23,6 +23,8 @@ resource "aws_vpc" "main" {
   }
 }
 
+
+# Public Subnets
 resource "aws_subnet" "public_a" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
@@ -88,15 +90,77 @@ resource "aws_route_table_association" "public_b" {
   route_table_id = aws_route_table.public.id
 }
 
-resource "aws_subnet" "private" {
+
+# Private Subnets
+resource "aws_subnet" "private_a" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.3.0/24"
-  availability_zone = "${var.region}b"
+  availability_zone = "${var.region}a"
 
   tags = {
-    Name       = "${var.env}-subnet-private"
+    Name       = "${var.env}-subnet-private-a"
     Owner      = var.owner
     Enviroment = var.env
     CostCenter = var.cost_center
   }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name       = "${var.env}-subnet-private-b"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name       = "${var.env}-eip"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_nat_gateway" "this" {
+  subnet_id = aws_subnet.public_a.id
+  allocation_id = aws_eip.nat.id
+  tags = {
+    Name       = "${var.env}-nat-gateway"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name       = "${var.env}-rtb-private"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_route_table_association" "private_a" {
+  subnet_id      = aws_subnet.private_a.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_route_table_association" "private_b" {
+  subnet_id      = aws_subnet.private_b.id
+  route_table_id = aws_route_table.private.id
 }
