bc1c3add2ef4419f6c4e0f8e654d652f9ccb859e
Pragma <pragma@192.168.1.6>
2024-06-25 10:58:09 -0500

    Creando Subnets Publicas

--- main.tf
+++ main.tf
@@ -1,13 +1,100 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
-resource "aws_vpc" "vpc" {
+resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
-    Name = "${var.env}-vpc-${var.cost_center}"
+    Name       = "${var.env}-vpc-${var.cost_center}"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${var.region}a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name       = "${var.env}-subnet-public-a"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "${var.region}b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name       = "${var.env}-subnet-public-b"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_internet_gateway" "this" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name       = "${var.env}-ig"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name       = "${var.env}-rtb-public"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.this.id
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "${var.region}b"
+
+  tags = {
+    Name       = "${var.env}-subnet-private"
     Owner      = var.owner
     Enviroment = var.env
     CostCenter = var.cost_center
--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,29 @@
 output "vpc_arn" {
   description = "ARN VPC"
-  value       = aws_vpc.vpc.arn
+  value       = aws_vpc.main.arn
 }
 
 output "vpc_id" {
   description = "ID VPC"
-  value       = aws_vpc.vpc.id
+  value       = aws_vpc.main.id
+}
+
+output "public_subnet_a_id" {
+  description = "ID public subnet A"
+  value       = aws_subnet.public_a.id
+}
+
+output "public_subnet_a_arn" {
+  description = "ARN public subnet A "
+  value       = aws_subnet.public_a.arn
+}
+
+output "public_subnet_b_id" {
+  description = "ID public subnet B"
+  value       = aws_subnet.public_b.id
+}
+
+output "public_subnet_b_arn" {
+  description = "ARN public subnet B "
+  value       = aws_subnet.public_b.arn
 }
