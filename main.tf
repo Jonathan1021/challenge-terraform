b25698fe55d8b50d9e4659058c8ff6205d0587f0
Pragma <pragma@192.168.1.6>
2024-06-25 12:08:13 -0500

    Module VPC Subnet Private

--- main.tf
+++ main.tf
@@ -74,75 +74,59 @@ resource "aws_route_table_association" "public" {
 }
 
 # # Private Subnets
-# resource "aws_subnet" "private_a" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.3.0/24"
-#   availability_zone = "${var.region}a"
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = element(concat(var.private_subnets, [""]), count.index)
+  availability_zone       = element(var.azs, count.index)
 
-#   tags = {
-#     Name       = "${var.env}-subnet-private-a"
-#     Owner      = var.owner
-#     Enviroment = var.env
-#     CostCenter = var.cost_center
-#   }
-# }
+  tags = {
+    Name       = "${var.env}-sb-private-${element(var.azs, count.index)}"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
 
-# resource "aws_subnet" "private_b" {
-#   vpc_id            = aws_vpc.main.id
-#   cidr_block        = "10.0.4.0/24"
-#   availability_zone = "${var.region}b"
+resource "aws_eip" "nat" {
+  domain = "vpc"
+  tags = {
+    Name       = "${var.env}-eip"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
 
-#   tags = {
-#     Name       = "${var.env}-subnet-private-b"
-#     Owner      = var.owner
-#     Enviroment = var.env
-#     CostCenter = var.cost_center
-#   }
-# }
+resource "aws_nat_gateway" "this" {
+  subnet_id     = aws_subnet.public[0].id
+  allocation_id = aws_eip.nat.id
+  tags = {
+    Name       = "${var.env}-nat-gateway"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
 
-# resource "aws_eip" "nat" {
-#   domain = "vpc"
-#   tags = {
-#     Name       = "${var.env}-eip"
-#     Owner      = var.owner
-#     Enviroment = var.env
-#     CostCenter = var.cost_center
-#   }
-# }
+resource "aws_route" "private_nat_gateway" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.this.id
+}
 
-# resource "aws_nat_gateway" "this" {
-#   subnet_id     = aws_subnet.public_a.id
-#   allocation_id = aws_eip.nat.id
-#   tags = {
-#     Name       = "${var.env}-nat-gateway"
-#     Owner      = var.owner
-#     Enviroment = var.env
-#     CostCenter = var.cost_center
-#   }
-# }
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name       = "${var.env}-rtb-private"
+    Owner      = var.owner
+    Enviroment = var.env
+    CostCenter = var.cost_center
+  }
+}
 
-# resource "aws_route" "private_nat_gateway" {
-#   route_table_id         = aws_route_table.private.id
-#   destination_cidr_block = "0.0.0.0/0"
-#   nat_gateway_id         = aws_nat_gateway.this.id
-# }
-
-# resource "aws_route_table" "private" {
-#   vpc_id = aws_vpc.main.id
-#   tags = {
-#     Name       = "${var.env}-rtb-private"
-#     Owner      = var.owner
-#     Enviroment = var.env
-#     CostCenter = var.cost_center
-#   }
-# }
-
-# resource "aws_route_table_association" "private_a" {
-#   subnet_id      = aws_subnet.private_a.id
-#   route_table_id = aws_route_table.private.id
-# }
-
-# resource "aws_route_table_association" "private_b" {
-#   subnet_id      = aws_subnet.private_b.id
-#   route_table_id = aws_route_table.private.id
-# }
+resource "aws_route_table_association" "private" {
+  count = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
